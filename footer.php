6eebdc0eb4049a39f265572feff3c1ba43be2b77
Nisuuu <nishaadhikari801@gmail.com>
2025-06-04 16:38:37 +0545

    made changes

--- footer.php
+++ footer.php
@@ -3,13 +3,12 @@
     <div class="footer_box">
       <div class="footer_logo_cont">
         <img src="book_logo.png" alt="">
-      <a href="home.php" class="book_logo">Bookiee</a>
-
+        <a href="home.php" class="book_logo">BookSathi</a>
       </div>
-      <p><i class="fas fa-phone"></i> 1234567890</p>
-      <p><i class="fas fa-envelope"></i> bookiee@gmail.com</p>
-      <p><i class="fas fa-map-marker-alt"></i> Mumbai, India - 902839</p>
-      <p><i class="fa-solid fa-shop"></i> Shop Timings : 9am - 9pm</p>
+      <p><i class="fas fa-phone"></i> +977-9812345678</p>
+      <p><i class="fas fa-envelope"></i> contact@booksathi.com</p>
+      <p><i class="fas fa-map-marker-alt"></i> Kathmandu, Nepal - 44600</p>
+      <p><i class="fa-solid fa-shop"></i> Shop Timings : 10am-8pm</p>
     </div>
 
     <div class="footer_box">
@@ -28,5 +27,5 @@
       <a href="orders.php">Orders</a>
     </div>
   </div>
-  <p>Copyright <i class="fa-regular fa-copyright"></i> 2023 <span>Tanisha Amin | All Rights Reserved.</span></p>
+  <p>&copy; 2025 <span>BookSathi | All Rights Reserved.</span></p>
 </section>
